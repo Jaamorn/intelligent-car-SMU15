e409a3c8e20a8e5b02486b3d32974bd8e1a5e27e
Jaamorn <x.jiachen@gmail.com>
2015-12-12 23:31:46 +0800

    注释掉LCD显示程序

--- main_program/App/main.c
+++ main_program/App/main.c
@@ -113,7 +113,7 @@ void  main(void)
         }
         
         /*************液晶屏显示程序**********/
-        
+        /*
         for(int j=0;j<CAMERA_R_H;j++)
         {
           for(int i=CAMERA_W;i>0;i--)
@@ -133,7 +133,7 @@ void  main(void)
           }
         }
         OLED_Refresh_Gram();
-        
+        */
         
         PORTC_ISFR = ~0;               //写1清中断标志位(必须的，不然回导致一开中断就马上触发中断)
         enable_irq(PORTC_IRQn);
